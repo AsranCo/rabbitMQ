0fcf635781005f04b010764ebd4c6f0d8efe639d
manavi <27414@iis.ir>
2020-02-21 19:18:00 +0330

    set slf4j

--- src/main/java/ir/marej/exchangeFanoutnoMissing/EmitLog.java
+++ src/main/java/ir/marej/exchangeFanoutnoMissing/EmitLog.java
@@ -2,9 +2,12 @@ package ir.marej.exchangeFanoutnoMissing;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import ir.marej.oneQueueoneConsumer.Consumer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EmitLog {
+    private static Logger logger = LogManager.getLogger(EmitLog.class);
 
     private final static String MESSAGE = "Hello Fanout Example";
 
@@ -18,7 +21,7 @@ public class EmitLog {
 
             channel.basicPublish(FanoutExchange.EXCHANGE_NAME, FanoutExchange.ROUTING_KEY, null, MESSAGE.getBytes());
 
-            System.out.println(" Message Sent '" + MESSAGE + "'");
+            logger.info(" Message Sent '" + MESSAGE + "'");
 
 
         }
